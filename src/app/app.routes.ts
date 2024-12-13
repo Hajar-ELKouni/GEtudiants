6cedb895958dd5f50310f7fe20c5970ccab76f6d
unknown <hajarelkouni1010@gmail.com>
2024-12-13 20:44:05 +0100

    Ajouter filiere

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { EtudiantComponent } from './etudiant/etudiant.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'etudiant',
+    component: EtudiantComponent,
+  },
+];
